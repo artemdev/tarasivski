e249eb30bab03d4b402acc93ffe9df813f4b2ad4
Artem Zymovets <artwayprojects@gmail.com>
2021-02-02 21:01:28 +0200

    changes

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,18 +1,18 @@
-// const modalBtn = document.querySelector('.about-us__link')
-// const closeBtn = document.querySelector('.modal__close-wrap')
-// const modalWindow = document.querySelector('.modal__window')
+const modalBtn = document.querySelector('.about-us__link')
+const closeBtn = document.querySelector('.modal__close-wrap')
+const modalWindow = document.querySelector('.modal__window')
 
-// const openModal = (e) => {
-//     console.log("it works!")
-//     e.preventDefault()
-//     modalWindow.style.display = "block";
-//     closeBtn.style.display = "flex";
-// }
-// const closeModal = (e) => {
-//     console.log("it works!")
-//     e.preventDefault()
-//     modalWindow.style.display = "none";
-//     closeBtn.style.display = "none";
-// }
-// modalBtn.addEventListener('click', openModal)
-// closeBtn.addEventListener('click', closeModal)
+const openModal = (e) => {
+    console.log("it works!")
+    e.preventDefault()
+    modalWindow.style.display = "block";
+    closeBtn.style.display = "flex";
+}
+const closeModal = (e) => {
+    console.log("it works!")
+    e.preventDefault()
+    modalWindow.style.display = "none";
+    closeBtn.style.display = "none";
+}
+modalBtn.addEventListener('click', openModal)
+closeBtn.addEventListener('click', closeModal)
